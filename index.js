062c40e9136b2a7d34e79531e1fe259f6744c51f
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2022-12-29 16:57:59 +0600

    MongoDB has connected and save user api created

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database Connection
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const db = client.db("taskQueryDB");
+    const usersCollection = db.collection("users");
+
+    //Save user With JWT authentication
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+
+      //token
+      const token = jwt.sign(user, process.env.ACCESS_KEY, { expiresIn: "1d" });
+
+      res.send({ result, token });
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running...on ${port}`);
+});
